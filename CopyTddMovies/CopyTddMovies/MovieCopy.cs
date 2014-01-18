186f04cde0a647bd83121e7df5e846cb79256b73
wwbbx <wwbbx@hotmail.com>
2014-01-18 15:29:36 +0800

    CopyTddMovies
    Tools to copy downloaded movies from TDDownload root folder to
    C:\Movies.
    
    Use this tool, I don't have to manually go through each folder to pick
    up mkv or rmvb files.

---
+++ CopyTddMovies/CopyTddMovies/MovieCopy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CopyTddMovies
+{
+    class MovieCopy
+    {
+        internal void Copy(string rootDir, string targetDir)
+        {
+            var movies = Directory.GetFiles(rootDir, "*", SearchOption.AllDirectories);
+
+            if(!DoIt)
+            {
+                Console.WriteLine("Demo Mode. Use '-doit' for real moving action.");
+            }
+
+            foreach(var movie in movies)
+            {
+                if (!IsMovieFile(movie)) continue;
+
+                var targetMovie = Path.Combine(targetDir, Path.GetFileName(movie));
+
+                if (DoIt)
+                {
+                    PrepareEnvironment(targetDir);
+                    File.Move(movie, targetMovie);
+                }
+
+                Console.WriteLine(string.Format("Movied {0}.", targetMovie));
+            }
+        }
+
+        private void PrepareEnvironment(string targetDir)
+        {
+            if (!Directory.Exists(targetDir)) Directory.CreateDirectory(targetDir);
+        }
+
+        private bool IsMovieFile(string movie)
+        {
+            if (movie.EndsWith("mkv")) return true;
+
+            if (movie.EndsWith("rmvb")) return true;
+
+            return false;
+        }
+
+        public bool DoIt { get; set; }
+    }
+}
---
+++ CopyTddMovies/CopyTddMovies/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyTddMovies
+{
+    class Program
+    {
+        static bool doIt = false;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if(!IsArgumentCorrect(args))
+                {
+                    ShowHelpMessage();
+                    return;
+                }
+
+                var copyer = new MovieCopy();
+
+                var rootDir = @"C:\TDDownload";
+                var targetDir = @"C:\Movies";
+
+                copyer.DoIt = doIt;
+
+                copyer.Copy(rootDir, targetDir);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+            }
+        }
+
+        private static void ShowHelpMessage()
+        {
+            Console.WriteLine("Copy Thunder downloaded movies to specified folder.");
+            Console.WriteLine("Please make sure Thunder is turned off.");
+            Console.WriteLine("Default source directory is C:\\TDDownload");
+            Console.WriteLine("Default output directory is C:\\Movies.");
+            Console.WriteLine("Default behavior will not copy it.");
+            Console.WriteLine("Specify '-doit' will let real copy happens.");
+        }
+
+        private static bool IsArgumentCorrect(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                if (args[0] == "-help") return false;
+
+                if (args[0] == "-doit") doIt = true;
+
+                return true;
+            }
+
+            return true;
+        }
+    }
+}
