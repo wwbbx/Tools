3fcfd422db76408329b0d1d36db096d5d0a500f9
wwbbx <wwbbx@hotmail.com>
2014-03-01 16:34:51 +0800

    ignore NUGet packages.

---
+++ PasswordGenius/PasswordGeniusModel/IDataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGeniusModel
+{
+    // DataService is the linkage between real data storage (DB or file)
+    // with upper level requirement by Genius.
+    // QueryString is JSON PasswordEntity as query setting.
+    // returned JSON will be converted as list of PasswordEntity.
+    public interface IDataService
+    {
+        List<PasswordEntity> Search(string jsonQueryString);
+
+        void Insert(PasswordEntity passwordEntityToInsert);
+    }
+}
---
+++ PasswordGenius/TestPasswordGenius/GeniusDataServiceFunction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestPasswordGenius
+{
+    [TestClass]
+    public class GeniusDataServiceFunction
+    {
+        [TestMethod]
+        public void search_return_one_qualified_result()
+        {
+
+        }
+
+        [TestMethod]
+        public void search_return_multiple_result()
+        {
+            Assert.Inconclusive("not implemented");
+
+        }
+
+        [TestMethod]
+        public void search_by_id_return_only_one_result()
+        {
+            Assert.Inconclusive("not implemented");
+
+        }
+
+        [TestMethod]
+        public void insert_will_store_data_into_storage()
+        {
+            Assert.Inconclusive("not implemented");
+
+        }
+    }
+}
